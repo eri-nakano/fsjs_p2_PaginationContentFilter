75e09d52cda9f0fb2349f15836ad880066c1f52c
Eri Nakano <33802747+eri-nakano@users.noreply.github.com>
2018-02-28 23:40:32 +0900

    Improve studentList and appendPageLinks
    -choose the studentList more specific
    -move showPage function from innerHTML to addEventListener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 function showPage(pageNumber, studentList) {
-  studentList = document.querySelectorAll('li');
+  studentList = Array.from(document.getElementsByClassName("student-item cf"));
 // first hide all students on the page
   $(studentList).hide();
 // Then loop through all students in our student list argument
@@ -12,20 +12,30 @@ function showPage(pageNumber, studentList) {
   } else {studentList[i].style.display = 'none'; } }
 };
 showPage(1,)
-function appendPageLinks(/* take a student list as an argument */studentList) {
-    studentList = document.querySelectorAll('li');
+function appendPageLinks(studentList) {
+    studentList = Array.from(document.getElementsByClassName("student-item cf"));
     // determine how many pages for this student list
     let totalPage = Math.ceil(studentList.length/10);
     // create a page link section
+    const pageLinkSection = document.createElement('ul')
         // “for” every page
     for (let i = 1; i <= totalPage; i += 1){
       let currentPage = i;
-      let pageLink = document.createElement('ul')
+      const pageLink = document.createElement('a');
       // add a page link to the page link section
-      pageLink = document.innerHTML='<li><a href="javascript:showPage('+ currentPage +')">' + currentPage + '</a><li>';
-      $('.pagination').append(pageLink);
+      pageLink.innerHTML = '<li><a href = "#">' + currentPage + '</a></li>';
+      pageLinkSection.classList.add('pagination');
+      $(pageLinkSection).append(pageLink);
+      // append our new page link section to the site
+      $('.page').append(pageLinkSection);
+      // define what happens when you click a link
+      pageLink.addEventListener('click', () => {
+        // Use the showPage function to display the page for the link clicked
+        showPage(i,studentList);
+      });
         // mark that link as “active”
-      $('.pagination .active').append(currentPage);
+          pageLink.classList.add('active');
+
 }
 }
 appendPageLinks();
