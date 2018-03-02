1b4c360261787207255a91b0bed9ba815dccd35b
Eri Nakano <33802747+eri-nakano@users.noreply.github.com>
2018-03-03 00:20:46 +0900

    changed pageLink element
    <a> to <li>

--- js/main.js
+++ js/main.js
@@ -18,24 +18,25 @@ function appendPageLinks(studentList) {
     let totalPage = Math.ceil(studentList.length/10);
     // create a page link section
     const pageLinkSection = document.createElement('ul')
+    pageLinkSection.classList.add('pagination');
         // “for” every page
     for (let i = 1; i <= totalPage; i += 1){
       let currentPage = i;
-      const pageLink = document.createElement('a');
+      const pageLink = document.createElement('li');
+      pageLink.innerHTML = '<a href = "#">' + currentPage + '</a>';
       // add a page link to the page link section
-      pageLink.innerHTML = '<li><a href = "#">' + currentPage + '</a></li>';
-      pageLinkSection.classList.add('pagination');
       $(pageLinkSection).append(pageLink);
+      $('.page').prepend(pageLinkSection);
       // append our new page link section to the site
       $('.page').append(pageLinkSection);
       // define what happens when you click a link
-      pageLink.addEventListener('click', () => {
+      pageLink.addEventListener = ('click', function() {
         // Use the showPage function to display the page for the link clicked
         showPage(i,studentList);
-      });
         // mark that link as “active”
-          pageLink.classList.add('active');
-
+        const currentLink = document.querySelectorAll('.pagination li a');
+        currentLink.classList.add('active');
+      });
 }
 }
 appendPageLinks();
